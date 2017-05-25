93ca4a26f48b0a07a0ee5f52f4b2e50b045d9659
Juan Chavez <jcchv92@gmail.com>
2017-05-25 08:44:26 -0600

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+        var http = require("http");
+        var handler = function(request, response) {
+            console.log("New request!");  
+            response.end("Hello World");
+        };
+        var server = http.createServer(handler);
+        server.listen(8080);
