bb29e6b5ea78517f398ae666968eed75376e19c7
Juan Chavez <jcchv92@gmail.com>
2017-05-25 09:23:17 -0600

    read file Sync Example

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
-        var http = require("http");
-        var handler = function(request, response) {
-            console.log("New request!");  
-            response.end("Hello World");
-        };
-        var server = http.createServer(handler);
+var http = require("http");
+var handler = function(request, response) {
+    console.log("New request!");  
+    response.end("Hello World");
+};
+var server = http.createServer(handler);
         server.listen(8080);
