776b38430b3e0fad346804b7c11a33c57ba4c7d6
Juan Chavez <jcchv92@gmail.com>
2017-05-26 15:11:26 -0600

    Add: Export module

--- form_params.js
+++ form_params.js
@@ -1,32 +1,28 @@
 var http = require("http"),
-    fs=require("fs");
+    fs = require("fs");
+parser = require("./params_parser.js");
+var p = parser.parse;
+http.createServer(function (req, res) {
+    if (req.url.indexOf("favicon.ico") > 0) {
+        return;
+    }
 
-http.createServer(function(req, res){
-    if(req.url.indexOf("favicon.ico") > 0){ return; }
-
-    fs.readFile("./index.html", function(err,html){
+    fs.readFile("./index.html", function (err, html) {
         var html_string = html.toString();
-        var arreglo_parametros=[], parametros= {};
         var variables = html_string.match(/[^\{\}]+(?=\})/g);
         var name = " ";
-        if(req.url.indexOf("?")>0){
-            var url_data = req.url.split("?");
-            var arreglo_parametros = url_data[1].split("&");
-        }
-        
-        for(var i = arreglo_parametros.length - 1; i>=0; i--){
-            var parametro = arreglo_parametros[i];
-            var param_data = parametro.split("=");
-            parametros[param_data[0]] = param_data[1];
+
+        var parametros = p(req);
+
+        for (var i = variables.length - 1; i >= 0; i--) {
+            var variable = variables[i];
+            html_string = html_string.replace("{" + variables[i] + "}", parametros[variable]);
         };
 
-        for(var i = variables.length - 1; i>=0; i--){
-            var variable = variables[i];
-            html_string=html_string.replace("{"+variables[i]+"}",parametros[variable]);
-        };
-        
-        res.writeHead(200,{"Content-Type":"text/html"});
-            res.write(html_string);
-            res.end();
+        res.writeHead(200, {
+            "Content-Type": "text/html"
         });
-    }).listen(8080);
+        res.write(html_string);
+        res.end();
+    });
+}).listen(8080);
--- render_vars.js
+++ render_vars.js
@@ -1,23 +1,25 @@
 var http = require("http"),
-    fs=require("fs");
+    fs = require("fs");
 
-http.createServer(function(req, res){
-    fs.readFile("./index.html", function(err,html){
+http.createServer(function (req, res) {
+    fs.readFile("./index.html", function (err, html) {
         var html_string = html.toString();
         //Regex searchs on HTML {x} variable
         var variables = html_string.match(/[^\{\}]+(?=\})/g);
         var name = "Juan Carlos";
         // varoables ['name']
-        for(var i = variables.length - 1; i>=0; i--){
+        for (var i = variables.length - 1; i >= 0; i--) {
             /*
             Executed to get the value of this variable
             */
-          var value = eval(variables[i]);
+            var value = eval(variables[i]);
             //Replacing {x} variable with var name
-            html_string=html_string.replace("{"+variables[i]+"}",value);
+            html_string = html_string.replace("{" + variables[i] + "}", value);
         };
         //Send Content
-        res.writeHead(200,{"Content-Type":"text/html"});
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
         res.write(html_string);
         res.end();
     });
