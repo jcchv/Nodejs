e76c21881c0a69dfce9e3af667f3297bc79b6b11
Juan Chavez <jcchv92@gmail.com>
2017-05-29 11:31:18 -0600

    Add: Hello World using Jade using variables in view

--- hola_express/app.js
+++ hola_express/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+app.set("view engine", "jade");
+
 app.get("/",function(req,res){
-    res.send("Hola Mundo");
+    res.render("index", {hola:"Hello Juan"});
 });
 app.listen(8080);
