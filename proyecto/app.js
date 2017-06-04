2af73a74c2060939b02870fbc86775e73828b436
Juan Chavez <jcchv92@gmail.com>
2017-06-03 18:52:38 -0600

    Add: Read parameters

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
-app.use("/estatico",express.static('public'));
+app.use("/public", express.static('public'));
 
-app.set("view engine","jade");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.set("view engine", "jade");
 
 app.get("/", function (req, res) {
     res.render("index");
@@ -12,4 +18,11 @@ app.get("/", function (req, res) {
 app.get("/login", function (req, res) {
     res.render("login");
 });
+
+app.post("/users", function (req, res) {
+    console.log("Password: " + req.body.password);
+    console.log("Email: " + req.body.email);
+    res.send("Data uploaded");
+});
+
 app.listen(8080);
