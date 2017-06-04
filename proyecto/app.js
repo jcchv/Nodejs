3cefed63462942fb99ee9c6edd727fe5cbbf8689
Juan Chavez <jcchv92@gmail.com>
2017-06-03 21:51:39 -0600

    Add: Mongo db implementation

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/curso_node");
+
+var userSchemaJSON = {
+    email: String,
+    password: String
+};
+
+var user_schema = new Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
 
 app.use("/public", express.static('public'));
 
@@ -16,13 +29,21 @@ app.get("/", function (req, res) {
 });
 
 app.get("/login", function (req, res) {
-    res.render("login");
+    User.find(function(err,doc){
+        console.log(doc);
+        res.render("login");
+    })
 });
 
 app.post("/users", function (req, res) {
-    console.log("Password: " + req.body.password);
-    console.log("Email: " + req.body.email);
-    res.send("Data uploaded");
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save(function () {
+        res.send("Data uploaded");
+    });
+
 });
 
 app.listen(8080);
