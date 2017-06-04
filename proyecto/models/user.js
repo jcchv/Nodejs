791777b4f2f99363e8169a0a538b9359ff12e3a3
Juan Chavez <jcchv92@gmail.com>
2017-06-03 22:02:37 -0600

    Add: Mongoose Schema Model

---
+++ proyecto/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var user_schema = new Schema({
+    name: String,
+    username: String,
+    password: String,
+    age: Number,
+    email: String,
+    date_of_birth: Date
+});
+
+/*
+Mongo Data Types
+    String
+    Number
+    Date
+    Buffer
+    Boolean
+    Mixed
+    Objectid
+    Array
+*/
